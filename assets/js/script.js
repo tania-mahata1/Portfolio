a98ff5c99300ab514d2cb3e517e1b35703ba7b64
tania-mahata1 <62003015+tania-mahata1@users.noreply.github.com>
2024-10-24 18:04:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development",  "web designing", "Data Analyst", "web development", "Backend skills"],
+    strings: ["Market research", "frontend development",  "web designing", "Data Analyst", "web development", "Backend skills"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
